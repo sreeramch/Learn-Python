3346984953cea928deec38058ebd4912d8cf5ea1
Sreeram Chinta <sreeram.chinta@gmail.com>
2020-04-15 12:43:22 -0500

    Create function.py
    First custom function definition

---
+++ function.py
@@ -0,0 +1,16 @@
+# More on functions inside a file
+
+'''
+By the way, functions/methods are the perfect way to make your programs modular,
+while also making the code easy on the eyes.
+
+Ideally, functions should do one specific thing which is unique to it.
+'''
+
+def printhere():
+	print("Hello inside the function printhere")
+
+
+# Invoke function call in the program
+printhere()
+
