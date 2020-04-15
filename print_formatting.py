8f75fa5a09f812dc2d8f0d5054f5e62207676c44
Sreeram Chinta <sreeram.chinta@gmail.com>
2020-04-15 12:36:22 -0500

    Create print_formatting.py
    print statement formatting demo

---
+++ print_formatting.py
@@ -0,0 +1,17 @@
+# Formatting arguments in print statement
+
+name = "Sreeram"
+lname = "Chinta"
+location = "Texas"
+year = 2020
+
+print()
+
+''' This is another way comments work in python..
+This is actually a multi-line comment.
+
+Context:
+print() is automatically wrapped by a newline
+'''
+print("%s %s lives in %s in %d" % (name, lname, location, year))
+
