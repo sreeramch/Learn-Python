69f81d55f81e1736cc1a9fb68e6ffc57e64f224f
Sreeram Chinta <sreeram.chinta@gmail.com>
2020-04-16 16:21:14 -0500

    Update files.py
    finally usage when not using with construct

--- files.py
+++ files.py
@@ -13,3 +13,14 @@ with open(filename) as fh:
 	str = fh.read()
 	print('Contents: ', str)
 
+print()
+# Without with constuct, the finally logic is required to close the file handle.
+try:
+	fh = open('c:/tmp/file.txt')
+except:
+	print('Could not open the file "c:/tmp/file.txt"')
+finally:
+	print('Closing the file handle')
+	fh.close()
+
+
